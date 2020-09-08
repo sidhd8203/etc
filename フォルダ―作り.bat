969c3528358d2f0038ebd203b8ada9cab46f0e78
DaengDaeng2 <50742110+sidhd8203@users.noreply.github.com>
2020-09-08 16:55:22 +0900

    Add files via upload

---
+++ フォルダ―作り.bat
@@ -0,0 +1,17 @@
+@echo off
+set /p a=日付を入力してください(mmdd)：
+echo %a%
+md PATH
+cd PATH
+md ファルダー名
+md ファルダー名
+md ファルダー名
+md ファルダー名
+md ファルダー名
+md ファルダー名
+md ファルダー名
+md 例）2020%a%_2145
+cd PATH
+md ファルダー名
+md ファルダー名
+
